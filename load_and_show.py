52c00510f8c55741aaa94bf2b3e4a478587878ed
Mark Shughart <mark@mindtribe.com>
2014-04-22 16:42:11 -0700

    These should have been included in the last commit

--- load_and_show.py
+++ load_and_show.py
@@ -1,1 +1,19 @@
-__author__ = 'mark'
+import argparse
+import numpy
+import matplotlib.pyplot as plt
+
+def load_csv(filename):
+    data = numpy.genfromtxt(filename, delimiter=',',skip_header=1)
+    return data
+
+# CSV is expected to have data labels in the first row, and data in all other rows
+def show_data(data):
+    print data
+    plt.plot(data[:,0], data[:,1:])
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-f', '--filename', dest='filename', help='Name of the CSV file to load')
+    args = parser.parse_args()
+    show_data(load_csv(args.filename))
--- quick_filters.py
+++ quick_filters.py
@@ -1,1 +1,35 @@
-__author__ = 'mark'
+import numpy
+import load_and_show
+import argparse
+
+#Quick FIR filter lowpass filter
+# First block number of values are unfiltered. This can be updated with a better solution later
+def running_avg(vector, block=4):
+    filtered_data = numpy.zeros(len(vector))
+    print range(0)
+    print range(1)
+    print range(2)
+    for i in xrange(len(vector)):
+        for j in xrange(min(block, i+1)):
+            filtered_data[i] += vector[i - j]
+        filtered_data[i] /= min(i+1, block)
+    #print filtered_data
+    return filtered_data
+
+#Quick IIR lowpass filter
+def exponential_filter(vector, alpha=0.8):
+    filtered_data = numpy.zeros(len(vector))
+    filtered_data[0] = vector[0]
+    for i in xrange(1,len(vector)):
+        filtered_data[i] = alpha * vector[i] + (1-alpha) * filtered_data[i-1]
+    #print filtered_data
+    return filtered_data
+
+if __name__ == "__main__":
+    test = [1,2,3,4,5,4,6,3,7,2,9,10,8,8,8,8,8]
+    time_test = range(len(test))
+    avg1 = running_avg(test,4)
+    avg2 = exponential_filter(test, 0.4)
+    data = time_test
+    data = numpy.column_stack((time_test, test, avg1, avg2))
+    load_and_show.show_data(data)
