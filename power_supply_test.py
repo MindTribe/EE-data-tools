e4a8f2a28a1aee86e3e78eefe836a9ff3957550e
Mark Shughart <mark@mindtribe.com>
2014-05-07 15:55:48 -0700

    with steps=2 and channels=2, graph shows expected data. Lots of magic numbers still.

--- power_supply_test.py
+++ power_supply_test.py
@@ -5,32 +5,34 @@ import time
 import load_and_show
 
 from picoscope import ps5000a
-# import Agilent PS
-
+import Agilent_E3631A
 
 if __name__ == "__main__":
 
     # Assuming a 2MHz switching f
-    SAMPLING_F = 10E6
+    SAMPLING_F = 5E6
     VOLT_RESOLUTION = 5
-
     CHANNELS = ['A', 'B', 'C', 'D']
 
+    # Command line parser
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-f', '--filename', dest='filename', help='Name of the CSV file to save',default='default.csv')
-    parser.add_argument('--low_voltage', dest='low_v', help='Starting voltage for input sweep', default=None, type=float)
-    parser.add_argument('--high_voltage', dest='high_v', help='Ending voltage for input sweep',default=None, type=float)
+    parser.add_argument('--low_voltage', dest='low_v', help='Starting voltage for input sweep', default=3.0, type=float)
+    parser.add_argument('--high_voltage', dest='high_v', help='Ending voltage for input sweep',default=3.1, type=float)
     parser.add_argument('--steps', dest='steps',help='Number of voltages tested between low and high', default=1, type=float)
     #parser.add_argument('--labels', dest='labels',help='Channel labels. Example   "Input_V,Output_V,Input_I"', default='')
     parser.add_argument('--channels', dest='channels', help='Number of channels. A=1, A and B = 2, etc...', default=1, type=int)
     args = parser.parse_args()
 
+    # Start the hardware
     scope = ps5000a.PS5000a(connect=False)
     serial = scope.enumerateUnits()
-
     scope = ps5000a.PS5000a(serial[0])
-    #print scope.getAllUnitInfo()
+    power_supply = Agilent_E3631A.AgilentE3631A()
+    print scope.getAllUnitInfo()
+    print power_supply.get_info()
 
+    # Set up sampling resolution
     res = scope.setSamplingFrequency(SAMPLING_F, 4096)
     sampleRate = res[0]
     res = scope.setSamplingFrequency(SAMPLING_F, res[1]/4)
@@ -41,7 +43,6 @@ if __name__ == "__main__":
     for count in xrange(args.channels):
         input_channels[CHANNELS[count]] = numpy.array(0)
 
-
     # set up scope channels
     scope.setResolution('12')
     for count in range(args.channels):
@@ -49,18 +50,15 @@ if __name__ == "__main__":
 
 
     # Run the acquisition loop
-    t = time.time()
-    for _ in xrange(1):
+    for volts in numpy.linspace(start=args.low_v, stop=args.high_v, num=args.steps):
         scope.runBlock()
+        power_supply.set_channel(power_supply.CHANNEL_P25V, voltage_limit=volts, current_limit=0.5)
+        power_supply.all_channels_on()
         while(scope.isReady() == False): time.sleep(0.01)
         for count in range(args.channels):
-            input_channels[CHANNELS[count]] = scope.getDataV(CHANNELS[count], res[1]/4)
-        scope.runBlock()
-        for count in range(args.channels):
-            numpy.append(input_channels[CHANNELS[count]], )
-        temp = time.time()
-        print temp-t
-        t = temp
+            input_channels[CHANNELS[count]] = numpy.append(input_channels[CHANNELS[count]], scope.getDataV(CHANNELS[count], res[1]/4))
+        power_supply.all_channels_off()
+
 
     # calculate time column
     # NOTE: There may be timing issues between each scope.runBlock() call. Need to confirm
@@ -72,32 +70,20 @@ if __name__ == "__main__":
         data = numpy.column_stack((data,input_channels[CHANNELS[count]]))
     data = numpy.column_stack((times,data))
 
-    #supply = ...()
-
-    #set up scope
-
-    #set up supply
-
-    #start recording
-
-    #for volts in numpy.linspace(args.low_v, args.high_v, args.steps, endpoint=True):
-        #change voltage
-        #enable
-    #    time.sleep(10)
-        #disable
 
     scope.close()
+    power_supply.all_channels_off()
     # Create the header
     header = 'Time'
     for count in xrange(args.channels):
         header = header + ',' + CHANNELS[count]
 
     # Save as CSV
-    numpy.savetxt(fname=args.filename , X=data, delimiter=',', header=header)
+    #numpy.savetxt(fname=args.filename , X=data, delimiter=',', header=header)
 
 
     # See data
-    load_and_show.show_data(data)
+    load_and_show.show_data(data[::4,:])
 
 
 
